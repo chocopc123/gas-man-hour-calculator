df55c14322c9a4fe53c392d49e9c41cbf5c7263d
choco <0215ryuuku@gmail.com>
2021-05-23 18:14:16 +0900

    設定ファイルの作成

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,26 +1,31 @@
 function calcManHour(manHourData, endTimeColumnName, categoryColumnName) {
   const usageJson = {};
   const colorJson = {};
-  let previousEndtime = '';
+  let previousEndtime = "";
   let first = true;
-  manHourData.forEach(function(data) {
-    if(!data.properties[endTimeColumnName].rich_text[0]) {
+  manHourData.forEach(function (data) {
+    if (!data.properties[endTimeColumnName].rich_text[0]) {
       return true;
     }
-    const endtime = data.properties[endTimeColumnName].rich_text[0].plain_text.split(':')
-    if(first) {
+    const endtime =
+      data.properties[endTimeColumnName].rich_text[0].plain_text.split(":");
+    if (first) {
       previousEndtime = endtime;
       first = false;
       return true;
     }
-    const usageHour = parseInt(endtime[0].replace(/[^0-9]/g, ''), 10) - parseInt(previousEndtime[0].replace(/[^0-9]/g, ''), 10);
-    const usageMinutes = parseInt(endtime[1].replace(/[^0-9]/g, ''), 10) - parseInt(previousEndtime[1].replace(/[^0-9]/g, ''), 10);
+    const usageHour =
+      parseInt(endtime[0].replace(/[^0-9]/g, ""), 10) -
+      parseInt(previousEndtime[0].replace(/[^0-9]/g, ""), 10);
+    const usageMinutes =
+      parseInt(endtime[1].replace(/[^0-9]/g, ""), 10) -
+      parseInt(previousEndtime[1].replace(/[^0-9]/g, ""), 10);
     previousEndtime = endtime;
 
     const usage = usageHour * 60 + usageMinutes;
     const category = data.properties[categoryColumnName].select.name;
     const color = data.properties[categoryColumnName].select.color;
-    if(usageJson[category]){
+    if (usageJson[category]) {
       usageJson[category] += usage;
     } else {
       usageJson[category] = usage;
@@ -31,7 +36,7 @@ function calcManHour(manHourData, endTimeColumnName, categoryColumnName) {
 }
 
 function convertStringTime(json) {
-  Object.keys(json).forEach(function(key) {
+  Object.keys(json).forEach(function (key) {
     const hour = Math.floor(json[key] / 60);
     const rem = json[key] % 60;
     const stringTime = createStringTime(hour, rem);
@@ -41,9 +46,9 @@ function convertStringTime(json) {
 }
 
 function createStringTime(hour, rem) {
-  const stringHour = hour + 'h';
-  if(rem !== 0) {
-    return stringHour + rem + 'm';
+  const stringHour = hour + "h";
+  if (rem !== 0) {
+    return stringHour + rem + "m";
   } else {
     return stringHour;
   }
